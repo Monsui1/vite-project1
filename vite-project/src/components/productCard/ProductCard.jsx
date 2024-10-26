bccedb1fa59f6c474a723aebe1f9c3e4e8b10873
Monsui <malowar@mail.ru>
2024-10-26 14:30:16 +0300

    Del comment

--- vite-project/src/components/productCard/ProductCard.jsx
+++ vite-project/src/components/productCard/ProductCard.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import "../productsPage/products.css";
 import { Link } from "react-router-dom";
-// import image from "./404.jpg";
 import replaceImage from "../replaceImage";
 
 export default function ProductCard({ id, name, price, image, rating }) {
-//   const state = {defaultImage: "/images/404.jpg"}
-//   const replaceImage = (error) => {
-//     //replacement of broken Image
-//     error.target.src = state.defaultImage;
-// }
+
   return (
     <Link to={id}>
       <div className="products__productcard">
         <div className="productcard__image">
           <img src={image} alt={name} width={250} height={250} onError={replaceImage}/>
-         {/* <img src={image} alt={name} width={250} height={250} /> */}
         </div>
         <div className="productcard__info">
           <p className="productcard__info__name" title={name}>
